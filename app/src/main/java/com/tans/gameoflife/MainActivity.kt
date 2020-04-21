c45233dffed616d857a5768be1c31de88cf0cd22
pengcheng.tan <pengcheng.tan@fenrir-inc.com.cn>
2020-04-21 20:36:54 +0800

    Add coroutine.

--- app/src/main/java/com/tans/gameoflife/MainActivity.kt
+++ app/src/main/java/com/tans/gameoflife/MainActivity.kt
@@ -2,11 +2,24 @@ package com.tans.gameoflife
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope by CoroutineScope(Dispatchers.Main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        launch {
+            repeat(20) {
+                delay(1000)
+                count_tv.text = (it + 1).toString()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        this.cancel()
     }
 }
