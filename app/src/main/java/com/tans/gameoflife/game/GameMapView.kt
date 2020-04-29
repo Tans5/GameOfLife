c0d5d1cf0c459b688da28df50d3bb278c5d16fb0
pengcheng.tan <pengcheng.tan@fenrir-inc.com.cn>
2020-04-29 18:39:41 +0800

    Update GameMapView.kt

--- app/src/main/java/com/tans/gameoflife/game/GameMapView.kt
+++ app/src/main/java/com/tans/gameoflife/game/GameMapView.kt
@@ -4,11 +4,9 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
-import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.View
 import androidx.core.view.ViewCompat
-import kotlin.math.max
 import kotlin.math.min
 
 class GameMapView : View {
@@ -18,6 +16,14 @@ class GameMapView : View {
         field = value
         ViewCompat.postInvalidateOnAnimation(this)
     }
+
+    var drawBorder: Boolean = false
+    set(value) {
+        field = value
+        ViewCompat.postInvalidateOnAnimation(this)
+    }
+
+    val borderSize: Int = 1
 
     val lineColor = Color.BLACK
 
@@ -49,31 +55,49 @@ class GameMapView : View {
 
     override fun onDraw(canvas: Canvas?) {
         val lifeModel = this.lifeModel
+        val drawBorder = this.drawBorder
+        val borderSize = if (drawBorder) {
+            borderSize
+        } else {
+            0
+        }
+        val mapSize = lifeModel?.mapSize
+        val life = lifeModel?.life
         if (canvas != null
-            && lifeModel != null
-            && lifeModel.life.size == lifeModel.mapSize.height * lifeModel.mapSize.width) {
-            val viewWidth = measuredWidth
-            val viewHeight = measuredHeight
-            val mapSize = lifeModel.mapSize
-            val cellSize = (min((viewWidth - 1).toFloat() / mapSize.width.toFloat(), (viewHeight - 1).toFloat() / mapSize.height.toFloat())).toInt()
+            && mapSize != null
+            && life != null
+            && life.size == mapSize.height * lifeModel.mapSize.width) {
+            val mapScreenWidth = measuredWidth.let { screenWidth ->
+                screenWidth - (screenWidth - borderSize) % mapSize.width
+            }
+            val mapScreenHeight = measuredHeight.let { screenHeight ->
+                screenHeight - (screenHeight - borderSize) % mapSize.height
+            }
+            val cellSize = min((mapScreenWidth - borderSize) / mapSize.width, (mapScreenHeight - borderSize) / mapSize.height) - borderSize
             if (cellSize >= 1) {
-                val mapWithInPixel = cellSize * mapSize.width + mapSize.width + 1
-                val mapHeightPixel = cellSize * mapSize.height + mapSize.height + 1
-                repeat(mapSize.width + 1) { i ->
-                    canvas.drawLine((i * (cellSize + 1)).toFloat(), 0f,
-                        (i * (cellSize + 1)).toFloat(), mapHeightPixel.toFloat(), linePaint)
-                }
+                if (drawBorder) {
+                    val mapWithInPixel = cellSize * mapSize.width + (mapSize.width + 1) * borderSize
+                    val mapHeightPixel = cellSize * mapSize.height + (mapSize.height + 1) * borderSize
+                    repeat(mapSize.width + 1) { i ->
+                        canvas.drawLine(
+                            (i * (cellSize + borderSize)).toFloat(), 0f,
+                            (i * (cellSize + borderSize)).toFloat(), mapHeightPixel.toFloat(), linePaint
+                        )
+                    }
 
-                repeat((mapSize.height + 1)) { i ->
-                    canvas.drawLine(0f, (i * (cellSize + 1)).toFloat(),
-                        mapWithInPixel.toFloat(), (i * (cellSize + 1)).toFloat(), linePaint)
+                    repeat((mapSize.height + 1)) { i ->
+                        canvas.drawLine(
+                            0f, (i * (cellSize + borderSize)).toFloat(),
+                            mapWithInPixel.toFloat(), (i * (cellSize + borderSize)).toFloat(), linePaint
+                        )
+                    }
                 }
 
                 lifeModel.life.withIndex().forEach { (i, isAlive) ->
                     if (isAlive) {
                         val (x, y) = mapSize.getCoordinate(i)
-                        canvas.drawRect((x * (cellSize + 1) + 1).toFloat(), (y * (cellSize + 1) + 1).toFloat(),
-                            (x * (cellSize + 1) + cellSize).toFloat(), (y * (cellSize + 1)  + cellSize).toFloat(), cellPaint)
+                        canvas.drawRect((x * (cellSize + borderSize) + borderSize).toFloat(), (y * (cellSize + borderSize) + borderSize).toFloat(),
+                            (x * (cellSize + borderSize) + cellSize).toFloat(), (y * (cellSize + borderSize)  + cellSize).toFloat(), cellPaint)
                     }
                 }
 
